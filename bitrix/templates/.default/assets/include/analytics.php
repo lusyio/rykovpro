7df521cd17af7db1650f54b929ec354b7049595e
Igor Garifyanov <garifianov.i@gmail.com>
2020-01-30 16:06:16 +0300

    Заменил скрипт яндекс.метрики

--- bitrix/templates/.default/assets/include/analytics.php
+++ bitrix/templates/.default/assets/include/analytics.php
@@ -1,31 +1,19 @@
 <!-- Yandex.Metrika counter -->
-<script type="text/javascript" >
-	(function (d, w, c) {
-			(w[c] = w[c] || []).push(function() {
-					try {
-							w.yaCounter21107527 = new Ya.Metrika({
-									id:21107527,
-									clickmap:true,
-									trackLinks:true,
-									accurateTrackBounce:true,
-									webvisor:true
-							});
-					} catch(e) { }
-			});
-
-			var n = d.getElementsByTagName("script")[0],
-					s = d.createElement("script"),
-					f = function () { n.parentNode.insertBefore(s, n); };
-			s.type = "text/javascript";
-			s.async = true;
-			s.src = "https://mc.yandex.ru/metrika/watch.js";
-
-			if (w.opera == "[object Opera]") {
-					d.addEventListener("DOMContentLoaded", f, false);
-			} else { f(); }
-	})(document, window, "yandex_metrika_callbacks");
+<script type="text/javascript"> (function (m, e, t, r, i, k, a) {
+        m[i] = m[i] || function () {
+            (m[i].a = m[i].a || []).push(arguments)
+        };
+        m[i].l = 1 * new Date();
+        k = e.createElement(t), a = e.getElementsByTagName(t)[0], k.async = 1, k.src = r, a.parentNode.insertBefore(k, a)
+    })(window, document, "script", "https://mc.yandex.ru/metrika/tag.js", "ym");
+    ym(21107527, "init", {
+        clickmap: true,
+        trackLinks: true,
+        accurateTrackBounce: true,
+        webvisor: true,
+        trackHash: true
+    });
 </script>
-<noscript><div><img src="https://mc.yandex.ru/watch/43681729" style="position:absolute; left:-9999px;" alt="" /></div></noscript>
 <!-- /Yandex.Metrika counter -->
 
 <!-- Facebook Pixel Code -->
