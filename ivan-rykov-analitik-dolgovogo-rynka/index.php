f927a8a3ff3cc5c20d4b85249c8ca2f0f33b893b
mastekator <akmiskam@yandex.ru>
2020-02-27 15:16:25 +0300

    страница рыков

---
+++ ivan-rykov-analitik-dolgovogo-rynka/index.php
@@ -0,0 +1,267 @@
+<?
+require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/header.php");
+$APPLICATION->SetPageProperty("TITLE", "Рыков Иван - антикризисный управляющий");
+$APPLICATION->SetTitle("ProРыков");
+$APPLICATION->SetAdditionalCSS("/ivan-rykov-analitik-dolgovogo-rynka/style.css");
+?>
+    <link rel="stylesheet" type="text/css"
+          href="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.css">
+
+    <div class="row rykov">
+        <div class="col-12">
+            <div>
+                <img src="/ivan-rykov-analitik-dolgovogo-rynka/img/rykov-info.jpg" alt="rykov-info">
+                <div class="rykov-info">
+                    <p class="rykov-info__title">Рыков Иван Юрьевич</p>
+                    <p class="rykov-info__description">Российский экономист, антикризисный управляющий, общественный
+                        деятель. Инициатор теории превентивного антикризисного управления как методологического подхода
+                        к финансовому оздоровлению. Председатель Учебно-методического совета Учебного центра
+                        профессионального развития, лидирующего учебного заведения по повышению квалификации арбитражных
+                        управляющих.</p>
+                    <div class="rykov-info__socials">
+                        <a href="https://www.facebook.com/rykovproff/?modal=suggested_action&notif_id=1579101163193967&notif_t=page_user_activity&ref=notif">
+                            <img src="/bitrix/templates/.default/components/bitrix/news/publications/bitrix/news.detail/.default/img/facebook-social.svg"
+                                 alt="facebook-social">
+                        </a>
+                        <a href="mailto:rykovpro@gmail.com">
+                            <img src="/bitrix/templates/.default/components/bitrix/news/publications/bitrix/news.detail/.default/img/gmail-social.svg"
+                                 alt="gmail-social">
+                        </a>
+                        <a href="https://instagram.com/rykov.pro">
+                            <img src="/bitrix/templates/.default/components/bitrix/news/publications/bitrix/news.detail/.default/img/instagram-social.svg"
+                                 alt="instagram-social">
+                        </a>
+                    </div>
+                </div>
+                <div class="rykov-top">
+                    <div>
+                        <p class="rykov-top__header">ТОП-<span>10</span></p>
+                        <p class="rykov-top__info">Входит в ТОП-10 антикризисных управляющих России</p>
+                    </div>
+                    <div>
+                        <p class="rykov-top__header">ТОП-<span>50</span></p>
+                        <p class="rykov-top__info">Основатель антикризисной юридической компании «Рыков групп», входящей
+                            в ТОП-50 крупнейших юридических компаний России.</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row areas">
+        <div class="col-12">
+            <p class="areas__header">Экспертные области</p>
+        </div>
+        <div class="col-lg-5 col-12 offset-0 offset-lg-1">
+            <div class="areas-card left">
+                <div class="areas-card__body">
+                    <p class="areas-card__content">Антикризисное управление и банкротство крупных предприятий и
+                        организаций в сфере строительства, транспорта, металлургии, производства и других отраслей</p>
+                </div>
+            </div>
+            <div class="areas-card mb">
+                <div class="areas-card__body">
+                    <p class="areas-card__content">Деятельность Федеральной службы судебных приставов, Федеральной
+                        налоговой службы</p>
+                </div>
+            </div>
+            <div class="areas-card">
+                <div class="areas-card__body">
+                    <p class="areas-card__content">Инвестиции и организация торгов в процедурах банкротства</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-5 col-12">
+            <div class="areas-card right">
+                <div class="areas-card__body">
+                    <p class="areas-card__content">Управление проблемными активами</p>
+                </div>
+            </div>
+            <div class="areas-card">
+                <div class="areas-card__body">
+                    <p class="areas-card__content">Банкротство граждан</p>
+                </div>
+            </div>
+            <div class="areas-card">
+                <div class="areas-card__body">
+                    <p class="areas-card__content">Взыскание дебиторской задолженности, деятельность коллекторов</p>
+                </div>
+            </div>
+            <div class="areas-card">
+                <div class="areas-card__body">
+                    <p class="areas-card__content">Субсидиарная ответственность по обязательствам должника</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row media">
+        <div class="col-lg-5 col-12 pr-2">
+            <p class="media__title">Сотрудничает с телеканалами и ведущими изданиями</p>
+            <p class="media__description">Многие ведущие российские СМИ регулярно обращаются к Ивану Рыкову за
+                комментарием или с предложение поучавствовать в программе по тематикам банкроства, взыскания долгов,
+                коллекторства и антикризисного управления.</p>
+            <p class="media__contact">По вопросам участия в мероприятиях, передачах, интервью: <a
+                        href="tel:+79266009241">Федоров Никон 8 (926) 600-92-41</a></p>
+        </div>
+        <div class="col-lg-6 offset-lg-1 col-12 offset-0">
+            <div class="slider-container">
+                <div id="carouselExampleControls" class="carousel slide" data-ride="carousel">
+                    <div class="carousel-inner">
+                        <div class="carousel-item active">
+                            <a data-fancybox href="https://www.youtube.com/watch?v=pV8VXymfbac">
+                                <div class="slider-container__img d-block w-100"
+                                     style="background-image: url(https://img.youtube.com/vi/pV8VXymfbac/0.jpg)">
+                                </div>
+                            </a>
+
+                        </div>
+                        <div class="carousel-item">
+                            <a data-fancybox href="https://www.youtube.com/watch?v=um5hhxXrsDE">
+                                <div class="slider-container__img d-block w-100"
+                                     style="background-image: url(https://img.youtube.com/vi/um5hhxXrsDE/0.jpg)">
+                                </div>
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a data-fancybox href="https://www.youtube.com/watch?v=MEyLxzpvGCI">
+                                <div class="slider-container__img d-block w-100"
+                                     style="background-image: url(https://img.youtube.com/vi/MEyLxzpvGCI/0.jpg)">
+                                </div>
+                            </a>
+                        </div>
+                        <div class="carousel-item">
+                            <a data-fancybox href="https://www.youtube.com/watch?v=3EXsvJA7cJE">
+                                <div class="slider-container__img d-block w-100"
+                                     style="background-image: url(https://img.youtube.com/vi/3EXsvJA7cJE/0.jpg)">
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <a class="carousel-control-prev" href="#carouselExampleControls" role="button" data-slide="prev">
+                        <img src="/ivan-rykov-analitik-dolgovogo-rynka/svg/prev.svg" alt="prev">
+                    </a>
+                    <a class="carousel-control-next" href="#carouselExampleControls" role="button" data-slide="next">
+                        <img src="/ivan-rykov-analitik-dolgovogo-rynka/svg/next.svg" alt="next">
+                    </a>
+                </div>
+            </div>
+        </div>
+        <div class="col-12 media__firms">
+            <img class="media__firm" src="/ivan-rykov-analitik-dolgovogo-rynka/svg/first-channel.svg"
+                 alt="pervyj-kanal">
+            <img class="media__firm" src="/ivan-rykov-analitik-dolgovogo-rynka/svg/ntv.svg" alt="ntf">
+            <img class="media__firm" src="/ivan-rykov-analitik-dolgovogo-rynka/svg/rbk.svg" alt="rbk">
+            <img class="media__firm" src="/ivan-rykov-analitik-dolgovogo-rynka/svg/vedomosti.svg" alt="vedomosti">
+            <img class="media__firm" src="/ivan-rykov-analitik-dolgovogo-rynka/svg/360.svg" alt="360-kanal">
+            <img class="media__firm" src="/ivan-rykov-analitik-dolgovogo-rynka/svg/forbes.svg" alt="forbes">
+            <img class="media__firm" src="/ivan-rykov-analitik-dolgovogo-rynka/svg/otr.svg" alt="otr">
+            <img class="media__firm mr-0" src="/ivan-rykov-analitik-dolgovogo-rynka/svg/kommersant.svg"
+                 alt="kommersant">
+        </div>
+    </div>
+
+    <div class="row activity">
+        <div class="col-12">
+            <p class="activity__header">Общественная деятельность</p>
+        </div>
+        <div class="col-lg-5 offset-lg-1 col-12 offset-0">
+            <div class="activity-card">
+                <div class="activity-card__body">
+                    <img class="activity-card__header" src="/ivan-rykov-analitik-dolgovogo-rynka/img/palata-rf.png"
+                         alt="palata-rf">
+                    <p class="activity-card__content">Председатель Комиссии по финансовой безопасности Совета по
+                        финансово-промышленной и инвестиционной политике Торгово-промышленной палаты РФ</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-5 col-12">
+            <div class="activity-card">
+                <div class="activity-card__body">
+                    <img class="activity-card__header" src="/ivan-rykov-analitik-dolgovogo-rynka/img/sovet-po-prof.png"
+                         alt="sovet-po-prof">
+                    <p class="activity-card__content">Член рабочей группы по развитию квалификаций арбитражных
+                        управляющих Совета по профессиональным квалификациям по финансовым рынкам.</p>
+                </div>
+            </div>
+        </div>
+
+        <div class="col-lg-5 offset-lg-1 col-12 offset-0">
+            <div class="activity-card">
+                <div class="activity-card__body">
+                    <img class="activity-card__header"
+                         src="/ivan-rykov-analitik-dolgovogo-rynka/img/chlen-prezidiuma.png" alt="chlen-prezidiuma">
+                    <p class="activity-card__content">Член Президиума Национального союза профессионалов антикризисного
+                        управления.</p>
+                </div>
+            </div>
+        </div>
+        <div class="col-lg-5 col-12">
+            <div class="activity-card">
+                <div class="activity-card__body">
+                    <img class="activity-card__header"
+                         src="/ivan-rykov-analitik-dolgovogo-rynka/img/zamestitel-predsedatelya.png"
+                         alt="zamestitel-predsedatelya">
+                    <p class="activity-card__content">Первый заместитель Председателя Общероссийского профессионального
+                        союза арбитражных управляющих.</p>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <div class="row books">
+        <div class="col">
+            <p class="books__header">Научная и публицистическая деятельность</p>
+            <div class="books-container">
+                <a href="/books">
+                    <img src="/ivan-rykov-analitik-dolgovogo-rynka/img/skhemy-bolshe-ne-rabotayut.png"
+                         alt="skhemy-bolshe-ne-rabotayut">
+                    <img class="more" src="/ivan-rykov-analitik-dolgovogo-rynka/svg/more.svg" alt="more">
+                </a>
+                <a href="/books">
+                    <img src="/ivan-rykov-analitik-dolgovogo-rynka/img/biznes-na-rasprodazhah-imushchestva-banktrotov.png"
+                         alt="biznes-na-rasprodazhah-imushchestva-banktrotov">
+                    <img class="more" src="/ivan-rykov-analitik-dolgovogo-rynka/svg/more.svg" alt="more">
+                </a>
+                <a href="/books">
+                    <img src="/ivan-rykov-analitik-dolgovogo-rynka/img/subsidiarnaya-otvetstvennost.png"
+                         alt="subsidiarnaya-otvetstvennost">
+                    <img class="more" src="/ivan-rykov-analitik-dolgovogo-rynka/svg/more.svg" alt="more">
+                </a>
+                <a href="/books">
+                    <img src="/ivan-rykov-analitik-dolgovogo-rynka/img/kreditory-vs-debitory.png"
+                         alt="kreditory-vs-debitory">
+                    <img class="more" src="/ivan-rykov-analitik-dolgovogo-rynka/svg/more.svg" alt="more">
+                </a>
+            </div>
+        </div>
+    </div>
+
+    <div class="row publications">
+        <div class="col-12">
+            <p class="publications__header">Список научных публикаций в журналах ВАК</p>
+        </div>
+        <div class="col-lg-10 offset-lg-1 col-12 offset-0">
+            <ol class="publications__list">
+                <li>Системные аспекты морфологии переходных кризисных явлений в организации – Рыков И.Ю. //
+                    Научно-практический журнал Эффективное антикризисное управление. – 2016 г. – №5 (98) – стр.50-53
+                </li>
+                <li>Использование методов кластерного анализа финансовых показателей деятельности организации в
+                    превентивном антикризисном управлении – Рыков И.Ю. // Научно-теоретический журнал ВЕСТНИК РУК – 2016
+                    г. – №3 (25) – стр.83-88
+                </li>
+                <li>Фазовый и стохастический подходы к антикризисному управлению в социально-экономических системах –
+                    Рыков И.Ю. // Научно-теоретический журнал ВЕСТНИК РУК – 2017 г. – №1 (27) – стр.45-50
+                </li>
+                <li>Теоретико-методические подходы к определению текущего этапа развития организации и предпосылок к
+                    переходным периодам в превентивном антикризисном управлении – Рыков И.Ю. // Журнал ВЕСТНИК ПВГУС
+                    ЭКОНОМИКА – 2017 – №1 (47) – стр. 125-130
+                </li>
+                <li>Консалтинг в Российской Федерации:формирование, развитие, перспективы – Рыков И.Ю. //
+                    Научно-теоретический журнал ВЕСТНИК РУК – 2018 г. –№ 2(32) 2018 – стр. 70-74
+                </li>
+            </ol>
+        </div>
+    </div>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/fancybox/3.5.7/jquery.fancybox.min.js"></script>
+<? require($_SERVER["DOCUMENT_ROOT"] . "/bitrix/footer.php"); ?>
